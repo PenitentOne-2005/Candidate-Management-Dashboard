ed85602417f9949f1d321b50cc28840984d86508
PenitentOne-2005 <pozdrakovzena@gmail.com>
2026-01-21 11:27:15 +0300

    Update baseURL

--- client/src/api/candidates.ts
+++ client/src/api/candidates.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import type { Candidate, StatusType } from "@/types";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
 export const getCandidates = async (): Promise<Candidate[]> => {
